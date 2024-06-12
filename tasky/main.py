b91ef6fc3a788295ca1fff2ebd5f500aeb7e384f
Akhil Unnikrishnan <iamakhiluk@gmail.com>
2024-06-12 14:34:19 +0530

    Adds CORS-related configuration

--- tasky/main.py
+++ tasky/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from tasky.routers import users, auth, tasks, lists
-# from tasky.config import settings
 
 app = FastAPI()
 
@@ -8,3 +7,7 @@ app.include_router(lists.router)
 app.include_router(tasks.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+
+origins = ['*']
+
+app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials = True, allow_methods = ['*'], allow_headers = ['*'])
